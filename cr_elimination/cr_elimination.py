436bad2f5ed7a5c7a510fc664ba322c1e8094139
Terry Cox <terrycox@TerryNewMac.local>
2020-03-27 17:46:22 -1000

    first commit

---
+++ cr_elimination/cr_elimination.py
@@ -0,0 +1,150 @@
+import numpy as np
+# from astropy.io import fits
+# from astropy.utils.data import get_pkg_data_filename
+from astropy.visualization import ZScaleInterval, AsinhStretch,SinhStretch, BaseStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
+from keras.models import load_model, model_from_json
+import matplotlib
+from matplotlib import pyplot as plt
+from model.create_model import create_model
+import sys
+sys.setrecursionlimit(10**8)
+
+class Cosmic_Ray_Elimination(object):
+    def __init__(self):
+
+        try:
+            json_file = open('model.json', 'r')
+            loaded_model_json = json_file.read()
+            json_file.close()
+            loaded_model = model_from_json(loaded_model_json)
+            # load weights into new model
+            loaded_model.load_weights("model.h5")
+            self.model = loaded_model
+        except:
+            self.model = create_model()
+
+    def remove_cosmic_rays(self, image_data, zscore = 2, pixels_shift = 256):
+
+        try:
+            image_data = np.array(image_data)
+            if image_data.ndim != 2:
+                print('Needs to be a 2D array.')
+                return None
+            elif image_data.dtype != np.dtype('float'):
+                print('Needs to be a 2D array of floats not', image_data.dtype)
+                return None
+            elif np.min(image_data.shape) < 256:
+                print('\'image_data\' must have a width and length >= 256: ', image_data.shape)
+                return None
+        except:
+            print('Needs to be a 2D array.')
+            return None
+
+        if not isinstance(zscore, (int, float)):
+            print('\'zscore\' needs to be type int or float.')
+            return None
+
+        if not isinstance(pixels_shift, int):
+            print('\'pixels_shift\' needs to be type int.')
+            return None
+
+        if pixels_shift > 256:
+            print('\'pixels_shift\' can be a max of 256.')
+            print('pixels_shift = 256')
+            pixels_shift = 256
+
+        ############################### Load and Scale ############################################
+        max_pixel = np.max(image_data)
+        if max_pixel > 1:
+            interval = ZScaleInterval()
+            vmin,vmax = interval.get_limits(image_data)
+            norm = ImageNormalize(vmin=vmin,vmax=vmax,stretch=SinhStretch())
+            scale = norm.__call__(image_data).data
+        else:
+            scale = image_data
+
+        z = np.mean(scale)+np.std(scale)*zscore
+        if z > 1:
+            z = 1
+        scale_binary = scale.copy()
+        scale_binary[scale_binary >= z] = 1
+        scale_binary[scale_binary < z] = 0
+
+        ############################### Build X ############################################
+
+        x1 = np.array(range(0,int(np.ceil(scale.shape[0]/pixels_shift))))*pixels_shift
+        x2 = np.array(range(0,int(np.ceil(scale.shape[1]/pixels_shift))))*pixels_shift
+        sixteenth = np.round(np.array(range(0,4))*64)
+
+        scale_shape = scale.shape
+        X = []
+        image256pos = []
+        images_orig256 = []
+        for i in x1:
+            for j in x2:
+                if i+256 > scale_shape[0]-1 and j+256 > scale_shape[1]-1:
+                    image256 = scale_binary[(scale_shape[0]-256):(scale_shape[0]), (scale_shape[1]-256):(scale_shape[1])]
+                    image256pos.append((scale_shape[0]-256, scale_shape[1]-256))
+                elif i+256 > scale_shape[0]-1:
+                    image256 = scale_binary[(scale_shape[0]-256):(scale_shape[0]), j:(j+256)]
+                    image256pos.append((scale_shape[0]-256, j))
+                elif j+256 > scale_shape[1]-1:
+                    image256 = scale_binary[i:(i+256), (scale_shape[1]-256):(scale_shape[1])]
+                    image256pos.append((i, scale_shape[1]-256))
+                else:
+                    image256 = scale_binary[i:(256+i), j:(256+j)]
+                    image256pos.append((i,j))
+                images_orig256.append(image256)
+                for z in sixteenth:
+                    for k in sixteenth:
+                        x = image256[z:(z+64), k:(k+64)].reshape(64,64,1)
+                        X.append(x)
+
+        ############################### Predictions ############################################
+        X = np.array(X)
+        Y_pred = self.model.predict(X)
+        counter = 0
+
+        keepers = np.zeros(scale.shape)
+        counter_matrix = np.zeros(scale.shape)
+        for i, point in enumerate(image256pos):
+            pred_data_y = np.zeros((256,256))
+
+            for j in sixteenth:
+                for k in sixteenth:
+                    pred_data_y[j:(j+64), k:(k+64)] = Y_pred[counter].reshape(64,64).round()
+                    counter += 1
+
+            keepers[point[0]:(point[0]+256), point[1]:(point[1]+256)] = keepers[point[0]:(point[0]+256), point[1]:(point[1]+256)] + pred_data_y
+            counter_matrix[point[0]:(point[0]+256), point[1]:(point[1]+256)] += 1
+        keepers = keepers/counter_matrix
+        keepers_binary = np.ceil(keepers/np.max(keepers))
+
+        ############################### Fill In Gaps ############################################
+        points = zip(*np.where(keepers_binary == 1))
+
+        for point in points:
+            keepers_binary = self.fill_space(point, keepers_binary, scale_binary)
+
+        ############################### Remove and Finish ############################################
+        remove_binary = scale_binary - keepers_binary
+        new_image_data = scale  - scale*remove_binary
+
+        return new_image_data
+
+
+    def fill_space(self, point, keepers_binary, scale_binary):
+        for i in [point[0]-1,point[0],point[0]+1]:
+            for j in [point[1]-1,point[1],point[1]+1]:
+                try:
+                    if keepers_binary[i,j] == 0 and scale_binary[i,j] == 1 and (i,j) != point:
+                        keepers_binary[i,j] = 1
+                        return self.fill_space((i,j), keepers_binary, scale_binary)
+                    elif keepers_binary[i,j] == 1 and scale_binary[i,j] == 0:
+                        keepers_binary[i,j] = 0
+                        return keepers_binary
+                except:
+                    #print('(%s, %s) is out of bounds'%(i,j))
+                    continue
+        return keepers_binary
---
+++ cr_elimination/model/create_model.py
@@ -0,0 +1,68 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from keras import layers
+import numpy as np
+from PIL import Image
+from zipfile import ZipFile
+import keras.backend as K
+
+def custom_loss_function(y_actual, y_predicted):
+    n11 = K.sum(K.round(y_actual) * K.round(y_predicted))
+    nboth = K.sum(K.round(y_actual))
+    n10 = nboth - n11
+
+    mse = K.mean(K.square(y_actual-y_predicted))
+    custom_loss_value = mse * (1 + K.sum(n10)/nboth)
+    return custom_loss_value
+
+def create_model():
+
+    Y = []
+    X_full = []
+    sixteenth = np.round(np.array(range(0,4))*64)
+
+    filename = 'Original.zip'
+    with ZipFile(filename) as archive:
+        for entry in archive.infolist():
+            with archive.open(entry) as file:
+                img = Image.open(file).convert('L')
+                data_x = np.floor(np.array(img.getdata())/255).reshape(256,256)
+                for j in sixteenth:
+                    for k in sixteenth:
+                        X_full.append(data_x[j:(j+64), k:(k+64)])
+
+    filename = 'Solutions.zip'
+    with ZipFile(filename) as archive:
+        for entry in archive.infolist():
+            with archive.open(entry) as file:
+                img = Image.open(file).convert('L')
+                data_y = np.floor(np.array(img.getdata())/255).reshape(256,256)
+                for j in sixteenth:
+                    for k in sixteenth:
+                        Y.append(data_y[j:(j+64), k:(k+64)].reshape(1,-1)[0])
+
+    X_full = np.array(X_full).reshape((len(X_full), 64,64,1))
+    Y = np.array(Y)
+
+    # define the keras model
+    model = Sequential()
+
+    model.add(layers.Conv2D(32,(5,5),activation='relu', input_shape=(64,64,1))) # 32 (5,5)
+    model.add(layers.MaxPooling2D((3, 3))) # Max (3,3)
+    model.add(layers.Conv2D(64, (5, 5), activation='relu')) # 64 (5,5)
+    model.add(layers.MaxPooling2D((2, 2))) # Max (2,2)
+    model.add(layers.Flatten())
+
+    model.add(Dense(6000, activation='relu')) # 6000
+    model.add(Dense(len(Y[0]), activation='relu'))
+
+    model.compile(loss=custom_loss_function, optimizer='adam', metrics=['mse','mae'])
+    history = model.fit(X_full, Y, epochs=20, verbose = 1)
+
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights("model.h5")
+
+    return model
