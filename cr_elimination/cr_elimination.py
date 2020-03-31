4df8f5a835ea0d48b610672bb4de4f0149f59c79
Terry Cox <terrycox@TerryNewMac.local>
2020-03-30 18:10:51 -1000

    documented methods

--- cr_elimination/cr_elimination.py
+++ cr_elimination/cr_elimination.py
@@ -1,6 +1,13 @@
+# Author: Terry Cox
+# GitHub: https://github.com/Terry071896/Cosmic_Ray_Elimination
+# Email: tcox@keck.hawaii.edu, tfcox1703@gmail.com
+
+__author__ = ['Terry Cox']
+__version__ = '1.0.1'
+__email__ = ['tcox@keck.hawaii.edu', 'tfcox1703@gmail.com']
+__github__ = 'https://github.com/Terry071896/Cosmic_Ray_Elimination'
+
 import numpy as np
-# from astropy.io import fits
-# from astropy.utils.data import get_pkg_data_filename
 from astropy.visualization import ZScaleInterval, AsinhStretch,SinhStretch, BaseStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
 from keras.models import load_model, model_from_json
@@ -11,13 +18,36 @@ import sys
 sys.setrecursionlimit(10**8)
 
 class Cosmic_Ray_Elimination(object):
+    '''
+    A class built to use machine learning to eliminate cosmic rays from spectral images.
+
+    ...
+
+    Attributes
+    ----------
+    model : Keras Model
+        the convolutional neural network to remove cosmic rays from a spectrial image.
+    new_image_data : numpy.ndarray (2D)
+        a NumPy array of the spectral image without cosmic rays.
+    scale : numpy.ndarray (2D)
+        a scaled version of the original spectral image (if required)
+    remove_binary : numpy.ndarray (2D)
+        a binary matrix of the what pixel values are removed (1 = removed, 0 = kept)
+
+    Methods
+    -------
+    remove_cosmic_rays(image_data, zscore = 2, pixels_shift = 256)
+        the method that removes the cosmic rays from the spectrial image.
+    fill_gaps(point, keepers_binary, scale_binary)
+        a recursive function to clean up the values that were removed that shouldn't have been removed and vice versa.
+    '''
+
     def __init__(self):
         try:
             json_file = open('model/model.json', 'r')
             loaded_model_json = json_file.read()
             json_file.close()
             loaded_model = model_from_json(loaded_model_json)
-            # load weights into new model
             loaded_model.load_weights("model/model.h5")
             self.model = loaded_model
         except:
@@ -26,7 +56,53 @@ class Cosmic_Ray_Elimination(object):
             print('Model trained.')
 
     def remove_cosmic_rays(self, image_data, zscore = 2, pixels_shift = 256):
+        '''
+        The method that removes the cosmic rays from the spectrial image.
 
+        Parameters
+        ----------
+        image_data : numpy.ndarray (2D)
+            the original or scaled version of the original spectral image needing removal of cosmic rays.
+        zscore : int or float, optional (default = 2)
+            the number of standard deviations away from classifying a pixel value as 1 (above) or 0 (below).
+        pixels_shift : int, optional (default = 256)
+            the number pixels the 256 x 256 pixel window will slide from left to right.
+
+        Returns
+        -------
+        numpy.ndarray (2D)
+            a NumPy array of the spectral image without cosmic rays.
+
+        Notes
+        -----
+        The optional parameters, zscore and pixels_shift, are ment to allow for flexiblility of a given to remove more or less pixel values.
+
+        The closer zscore is to 0, the more pixels looked at for removial and naturally, the further from zero the less pixels (minumally the max pixels will be reviewed).
+        The speed of the process will take longer the more pixels are reviewed (maybe a second or 2).  It is recommended that the zscore is intended to find the outliers as cosmic rays tend to be.
+
+        zscore cutoff percentile chart:
+
+        zscore | percentile
+        -------|-----------
+           0   |    50%
+           1   |    84%
+           2   |    97.5%
+
+        As for pixels_shift, the closer to 1 the integer gets, the more frequent a pixel will be reviewed as the 256 window will slide by that many pixels.
+        For example,
+        If pixels_shift = 1 (minimum), then pixel:
+            (0,0) reviewed 1  -----> just in the first window (top right corner)
+            (0,1) reviewed 2  -----> in first and second windows; windows (0,0) and (0,1)
+            (1,1) reviewed 4  -----> in windows (0,0), (0,1), (1,0), and (1,1)
+            (0,356) reviewed 256 -----> in windows (0, 100-356)
+            (356,356) reviewed 65,526 -----> in windows (100-356, 100-356)
+        If pixels_shift = 256 (default and maximum), each pixel reviewed only ONCE.
+
+        The benefit of having a lower pixels_shift could be to allow the model to make another judgement at the pixel in a different spot on the 256 frame.
+        The problem with having lower pixels_shift values is the time increase as O(n^2) is very computationally expensive.
+
+        Most the time this will not help the accuracy of the model, so I recommend keeping the default.
+        '''
         try:
             image_data = np.array(image_data)
             if image_data.ndim != 2:
@@ -139,6 +215,34 @@ class Cosmic_Ray_Elimination(object):
 
 
     def fill_space(self, point, keepers_binary, scale_binary):
+        '''
+        A recursive function to clean up the values that were removed that shouldn't have been removed and vice versa.
+
+        Parameters
+        ----------
+        point : tuple or list
+            a kept point at which needs to check if the proximity points need to be turned on, off, or nothing.
+        keepers_binary : numpy.ndarray (2D)
+            a matrix of the predicted pixels that are ment to be kept. (Binary matrix)
+        scale_binary : numpy.ndarray (2D)
+            a matrix of the true pixels of the original image. A binary matrix where if above the zscore cutoff than 1 else 0.
+
+        Returns
+        -------
+        numpy.ndarray (2D)
+            this is the keepers_binary matrix with the adjusted pixel values that it touches (3 x 3 grid)
+
+        Notes
+        -----
+        This method is a recursive function that checks the points around a point to see if:
+            1. Is this point a False Negative (keepers_binary = 0 and scale_binary = 1)
+                Yes - Set to 1, then do the same with the points around this point.
+                No - Move on to next check
+            2. Is this point a False Positive (keepers_binary = 1 and scale_binary = 0)
+                Yes - Set to 0
+                No - Nothing
+            3. Return new keepers_binary matrix
+        '''
         try:
             if keepers_binary.shape != scale_binary.shape:
                 print('keepers_binary and scale_binary not the same size')
